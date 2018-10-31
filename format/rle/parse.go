cea100a6e54eeead35824aa1ddda1f54154f04aa
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2018-11-01 00:34:34 +0900

    Add rle format parser

---
+++ format/rle/parse.go
@@ -0,0 +1,121 @@
+package rle
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+func newCells(width, height int) [][]bool {
+	grid := make([][]bool, height)
+	for i := 0; i < height; i++ {
+		grid[i] = make([]bool, width)
+	}
+	return grid
+}
+
+func ParseFile(path string) ([][]bool, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return [][]bool{}, err
+	}
+	return Parse(f), nil
+}
+
+type RunLength struct {
+	RunCount  int
+	Tag       rune
+	IsNewLine bool
+}
+
+func Parse(r io.Reader) [][]bool {
+	br := bufio.NewReader(r)
+	var x, y int
+	var header string
+
+	for {
+		line, _, err := br.ReadLine()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		if line[0] == '#' {
+			continue
+		}
+
+		if line[0] == 'x' {
+			header = string(line)
+			break
+		}
+	}
+
+	_, err := fmt.Sscanf(header, "x = %d, y = %d", &x, &y)
+	if err != nil {
+		panic(err)
+	}
+
+	cells := newCells(x, y)
+
+	var runLengthes []RunLength
+
+	for {
+		line, _, err := br.ReadLine()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+		if line[0] == '#' {
+			continue
+		}
+
+		var parsingDigit string
+		for _, r := range string(line) {
+			if r == '!' {
+				break
+			}
+
+			if r == '$' {
+				runLengthes = append(runLengthes, RunLength{IsNewLine: true})
+				continue
+			}
+
+			if unicode.IsDigit(r) {
+				parsingDigit = parsingDigit + string(r)
+				continue
+			}
+
+			parsedInt := 1
+			if len(parsingDigit) > 0 {
+				parsedInt, _ = strconv.Atoi(parsingDigit)
+			}
+			parsingDigit = ""
+
+			runLengthes = append(runLengthes, RunLength{RunCount: parsedInt, Tag: r})
+		}
+	}
+
+	var i, j int
+	for _, runLength := range runLengthes {
+		if runLength.IsNewLine {
+			j++
+			i = 0
+			continue
+		}
+
+		if runLength.Tag == 'o' {
+			for p := 0; p < runLength.RunCount; p++ {
+				cells[j][i+p] = true
+			}
+		} else {
+			i = i + runLength.RunCount
+		}
+	}
+
+	return cells
+}
---
+++ format/rle/parse_test.go
@@ -0,0 +1,27 @@
+package rle
+
+import (
+	"os"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	f, err := os.Open("./test/glider.rle")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	b := Parse(f)
+	expected := [][]bool{
+		{false, true, false},
+		{false, false, true},
+		{true, true, true},
+	}
+
+	for i, row := range expected {
+		for j, expectedState := range row {
+			if b[i][j] != expectedState {
+				t.Errorf("Expected %v, But actually %v", expected, b)
+			}
+		}
+	}
+}
